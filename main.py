6da7619cd9933bba45da510c145c7eb1f139f309
AmitSingh12378 <amitgithubstartsingh@gmail.com>
2025-11-27 14:44:06 +0530

    after changes

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 print("name")
 
 print("after my new changes")
-print("after my new changes vs code arjun editing")
+print("after my new changes vs code arjun")
 print("after my new changes vs code amit editing")
